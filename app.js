fbead4f4d7d43bb1a38da06e8e4c6f83032712b0
Momo <mobuttondev@gmail.com>
2022-12-27 23:45:12 -0500

    connect to weathermap api

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
+const https = require("https");
 const app = express();
 
 app.get('/', function(req, res){
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=7c0316d54b5621aeed8c006867927d88&units=imperial";
+	https.get(url, function(response){
+		console.log(response.statusCode);
+		response.on("data", function(data){
+			const weatherData = JSON.parse(data);
+			const temp = weatherData.main.temp;
+			const description = weatherData.weather[0].description;
+			console.log(description);
+		})
+	})
 	res.send("Server is up and running");
 })
 
